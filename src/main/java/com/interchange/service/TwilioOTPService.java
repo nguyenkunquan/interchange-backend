d9eb194bae80934908b4335b8d884943dd3e73e2
= <=>
2024-02-22 12:58:46 +0700

    fix code

--- src/main/java/com/interchange/service/TwilioOTPService.java
+++ src/main/java/com/interchange/service/TwilioOTPService.java
@@ -2,7 +2,6 @@ package com.interchange.service;
 
 import com.interchange.config.TwilioConfig;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.twiml.voice.Sms;
 import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +15,8 @@ public class TwilioOTPService {
     private TwilioConfig twilioConfig;
 
     private String generateOTP;
+
+    private Random random = new Random();
     public void sendOTPForPasswordReset(String phoneNumber) {
 
         PhoneNumber to = new PhoneNumber(phoneNumber);
@@ -29,6 +30,6 @@ public class TwilioOTPService {
         return generateOTP.equals(enteredOTP);
     }
     private String generateOTP() {
-        return new DecimalFormat("000000").format(new Random().nextInt(999999));
+        return new DecimalFormat("000000").format(random.nextInt(999999));
     }
 }
--- src/main/java/com/interchange/service/UserDetailServiceImpl.java
+++ src/main/java/com/interchange/service/UserDetailServiceImpl.java
@@ -1,30 +1,24 @@
 package com.interchange.service;
 
-//import com.interchange.dao.UserDAO;
 import com.interchange.entities.User;
 import com.interchange.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
-//    private final UserDAO userDAO;
-    private final PasswordEncoder passwordEncoder;
+
 
     private final UserRepository userRepository;
 
-    public UserDetailServiceImpl(@Qualifier("bCryptPasswordEncoder") PasswordEncoder passwordEncoder, UserRepository userRepository) {
-//        this.userDAO = userDAO;
+    public UserDetailServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -36,11 +30,9 @@ public class UserDetailServiceImpl implements UserDetailsService {
         List<GrantedAuthority> grantList = new ArrayList<>();
         GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
         grantList.add(authority);
-        UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUserId(),
+
+        return  new org.springframework.security.core.userdetails.User(user.getUserId(),
                 user.getPassword(), grantList);
-//        System.out.println(passwordEncoder.encode(user.getPassword()));
-//        System.out.println(passwordEncoder.matches("minhbeo", user.getPassword()));
-        return userDetails;
     }
 
 
